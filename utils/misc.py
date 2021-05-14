9ff2dd72898b8735a5bcc6b20d634e40e4ac62d3
btmartin721 <evobio721@gmail.com>
2021-05-14 16:06:56 -0500

    Added progress bar function

--- utils/misc.py
+++ utils/misc.py
@@ -54,3 +54,16 @@ def timer(func):
 		print(f"Finshed {func.__name__!r} in {final_runtime}\n")
 		return value
 	return wrapper_timer
+
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+	count = len(it)
+	def show(j):
+		x = int(size*j/count)
+		file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+		file.flush()
+	show(0)
+	for i, item in enumerate(it):
+		yield item
+		show(i+1)
+	file.write("\n")
+	file.flush()
