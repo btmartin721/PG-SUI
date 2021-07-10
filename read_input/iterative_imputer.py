d68d0af50659b3b55c167ea8cd6823a08596e1e0
Bradley T. Martin <evobio721@gmail.com>
2021-07-10 16:22:34 -0500

    Removed ignore_warnings from fit_transform()

--- read_input/iterative_imputer.py
+++ read_input/iterative_imputer.py
@@ -942,3 +942,40 @@ class IterativeImputer(_BaseImputer):
 		"""
 		self.fit_transform(X)
 		return self
+
+def custom_plot_search_space(estimator, height=2, s=25, features: list = None):
+	"""
+	Parameters
+	----------
+	estimator: estimator object
+		A fitted estimator from :class:`~sklearn_genetic.GASearchCV`
+	height: float, default=2
+		Height of each facet
+	s: float, default=5
+		Size of the markers in scatter plot
+	features: list, default=None
+		Subset of features to plot, if ``None`` it plots all the features by default
+
+	Returns
+	-------
+	Pair plot of the used hyperparameters during the search
+
+	"""
+	sns.set_style("white")
+
+	df = logbook_to_pandas(estimator.logbook)
+	if features:
+		stats = df[features]
+	else:
+		variables = [*estimator.space.parameters, "score"]
+		stats = df[variables]
+
+	g = sns.PairGrid(stats, diag_sharey=False, height=height)
+	g = g.map_upper(sns.scatterplot, s=s, color="r", alpha=0.2)
+	g = g.map_lower(
+		sns.kdeplot,
+		shade=True,
+		cmap=sns.color_palette("ch:s=.25,rot=-.25", as_cmap=True),
+	)
+	g = g.map_diag(sns.kdeplot, shade=True, palette="crest", alpha=0.2, color="red")
+	return g
