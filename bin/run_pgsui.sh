b45ac411780013c7cb6c92cdbf65c4db70c0e114
btmartin721 <evobio721@gmail.com>
2022-10-08 01:23:28 -0500

    add site_rate file argument

--- bin/run_pgsui.sh
+++ bin/run_pgsui.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-python pgsui/pg_sui.py -p pgsui/example_data/phylip_files/test_n100.phy -m pgsui/example_data/popmaps/test.popmap -t pgsui/example_data/trees/test.tre -i pgsui/example_data/trees/test.iqtree
-            ]
+python pgsui/pg_sui.py -p pgsui/example_data/phylip_files/test_n100.phy -m pgsui/example_data/popmaps/test.popmap -t pgsui/example_data/trees/test.tre -i pgsui/example_data/trees/test.iqtree --site_rate pgsui/example_data/trees/test_n100.rate --prefix test
