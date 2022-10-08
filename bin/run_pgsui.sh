be3b097d99150ef8ab2b24b61bd4a60bba49e7a0
btmartin721 <evobio721@gmail.com>
2022-10-07 23:46:00 -0500

    script to run pgsui in linux

---
+++ bin/run_pgsui.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python pgsui/pg_sui.py -p pgsui/example_data/phylip_files/test_n100.phy -m pgsui/example_data/popmaps/test.popmap -t pgsui/example_data/trees/test.tre -i pgsui/example_data/trees/test.iqtree
+            ]
