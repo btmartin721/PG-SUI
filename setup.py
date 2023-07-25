829324a0ec373ec51c8fafa98993dcc52ad56d51
btmartin721 <evobio721@gmail.com>
2023-07-25 13:35:43 -0500

    version bump; reorganize keywords

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 NAME = "pg-sui"
-VERSION = "0.2.4"
+VERSION = "0.3"
 AUTHORS = "Bradley T. Martin and Tyler K. Chafin"
 AUTHOR_EMAIL = "evobio721@gmail.com"
 MAINTAINER = "Bradley T. Martin"
@@ -32,20 +32,22 @@ setup(
         "Bug Tracker": "https://github.com/btmartin721/PG-SUI/issues",
     },
     keywords=[
-        "python",
         "impute",
         "imputation",
         "imputer",
         "machine learning",
         "neural network",
+        "deep learning",
+        "python",
         "api",
-        "IterativeImputer",
         "vae",
+        "autoencoder",
         "ubp",
         "nlpca",
-        "autoencoder",
-        "deep learning",
         "population genomics",
+        "unsupervised",
+        "supervised",
+        "IterativeImputer",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -63,7 +65,7 @@ setup(
     ],
     license="GNU General Public License v3 (GPLv3)",
     packages=find_packages(),
-    python_requires=">=3.8,<4",
+    python_requires=">=3.8",
     install_requires=[
         "matplotlib",
         "seaborn",
