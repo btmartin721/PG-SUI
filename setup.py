fa48d1f3661e67aba3d96bebfd3b52c05e2d3607
btmartin721 <gh_email>
2023-09-12 01:28:03 -0500

    pandas <2.1.0; version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 NAME = "pg-sui"
-VERSION = "1.0.2"
+VERSION = "1.0.2.1"
 AUTHORS = "Bradley T. Martin and Tyler K. Chafin"
 AUTHOR_EMAIL = "evobio721@gmail.com"
 MAINTAINER = "Bradley T. Martin"
@@ -72,7 +72,7 @@ setup(
         "jupyterlab",
         "scikit-learn>=1.0",
         "tqdm",
-        "pandas",
+        "pandas<2.1.0",
         "numpy==1.24.3",
         "scipy",
         "xgboost",
