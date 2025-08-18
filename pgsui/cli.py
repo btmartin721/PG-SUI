d155e90ac5000f49acf6d180d1903345cfad6b06
btmartin721 <evobio721@gmail.com>
2025-08-18 12:01:13 -0400

    Refactor cli.py: Add NRemover2 import and update VCFReader usage; streamline data filtering process

--- pgsui/cli.py
+++ pgsui/cli.py
@@ -1,16 +1,23 @@
 import importlib.resources as pkg_resources
 import pprint
 
-from snpio import GenotypeEncoder, VCFReader
+from snpio import GenotypeEncoder, VCFReader, NRemover2
 
-from pgsui import ImputeAutoencoder, ImputeNLPCA, ImputeUBP, ImputeVAE
+from pgsui import (
+    ImputeAutoencoder,
+    ImputeHistGradientBoosting,
+    ImputeNLPCA,
+    ImputeRandomForest,
+    ImputeUBP,
+    ImputeVAE,
+)
 
 
 def main():
     # Locate the correct installed path for VCF and popmap files
     vcf_path = (
         pkg_resources.files("pgsui")
-        / "example_data/vcf_files/phylogen_subset14K.vcf_sorted.vcf.gz"
+        / "example_data/vcf_files/phylogen_subset14K.vcf.gz"
     )
     popmap_path = (
         pkg_resources.files("pgsui") / "example_data/popmaps/phylogen_nomx.popmap"
@@ -21,113 +28,68 @@ def main():
 
     # Pass absolute paths to VCFReader
     gd = VCFReader(
-        filename=str(vcf_path),
-        popmapfile=str(popmap_path),
+        filename=vcf_path,
+        popmapfile=popmap_path,
         force_popmap=True,
         verbose=True,
+        include_pops=["EA", "GU", "TT", "ON"],
     )
 
-    snp_data = gd.snp_data
+    nrm = NRemover2(gd)
 
-    ge = GenotypeEncoder(gd)
+    gd_filt = (
+        nrm.filter_missing_sample(0.8)
+        .filter_missing(0.5)
+        .filter_missing_pop(0.5)
+        .filter_monomorphic(exclude_heterozygous=True)
+        .filter_singletons(exclude_heterozygous=True)
+        .filter_biallelic(exclude_heterozygous=True)
+        .filter_mac(3)
+        .resolve()
+    )
 
     # ubp = ImputeUBP(
-    #     gd,
-    #     weights_temperature=1.0,
-    #     weights_normalize=False,
-    #     weights_alpha=2.0,
-    #     weights_log_scale=True,
-    #     tune=True,
-    #     tune_metric="pr_macro",
-    #     tune_n_trials=250,
-    #     model_early_stop_gen=20,
-    #     model_min_epochs=20,
-    #     model_validation_split=0.21,
-    #     model_learning_rate=0.0008886973483648648,
-    #     model_latent_dim=4,
-    #     model_hidden_activation="leaky_relu",
-    #     model_l1_penalty=0.0,
-    #     model_lr_patience=8,
-    #     model_dropout_rate=0.0,
-    #     model_device="cpu",
-    #     model_num_hidden_layers=8,
-    #     model_batch_size=32,
-    #     model_epochs=100,
-    #     model_gamma=2.0,
+    #     gd_filt,
     #     n_jobs=8,
     #     verbose=1,
-    #     seed=42,
-    #     sim_strategy="random_inv_multinom,
-    #     sim_prop_missing=0.3,
-    # )
-
-    # nlpca = ImputeNLPCA(
-    #     gd,
-    #     weights_temperature=1.0,
-    #     weights_normalize=False,
-    #     weights_alpha=2.0,
-    #     weights_log_scale=True,
-    #     tune=False,
-    #     tune_metric="pr_macro",
-    #     tune_n_trials=50,
-    #     model_early_stop_gen=20,
-    #     model_min_epochs=20,
-    #     model_validation_split=0.21,
-    #     model_learning_rate=0.00027532421267350253,
-    #     model_latent_dim=4,
-    #     model_hidden_activation="leaky_relu",
-    #     model_l1_penalty=0.0,
-    #     model_lr_patience=8,
-    #     model_dropout_rate=0.25,
-    #     model_device="cpu",
-    #     model_num_hidden_layers=1,
-    #     model_batch_size=32,
-    #     model_epochs=100,
-    #     model_gamma=2.0,
-    #     n_jobs=8,
-    #     verbose=1,
-    #     seed=42,
+    #     sim_prop_missing=0.5,
     #     sim_strategy="random_inv_multinom",
-    #     sim_prop_missing=0.3,
+    #     model_device="cpu",
+    #     model_dropout_rate=0.45,
+    #     model_hidden_activation="elu",
+    #     model_latent_dim=25,
+    #     model_learning_rate=0.001,
+    #     model_lr_input_factor=2.0,
+    #     model_gamma=1.0,
+    #     model_num_hidden_layers=4,
+    #     tune=True,
+    #     prefix="pgsui_ubp_test_noconv_2",
+    #     tune_n_trials=100,
+    #     model_batch_size=64,
+    #     weights_temperature=1.0,
+    #     weights_alpha=1.0,
+    #     weights_log_scale=False,
+    #     model_use_convolution=False,  # Set to True to use convolutional UBP
     # )
 
-    vae = ImputeAutoencoder(
-        gd,
-        weights_temperature=1.0,
-        weights_normalize=False,
-        weights_alpha=2.0,
-        weights_log_scale=True,
-        tune=False,
-        tune_metric="pr_macro",
-        tune_n_trials=50,
-        model_early_stop_gen=20,
-        model_min_epochs=20,
-        model_learning_rate=0.00027532421267350253,
-        model_latent_dim=4,
-        model_hidden_activation="leaky_relu",
-        model_l1_penalty=0.0,
-        model_lr_patience=8,
-        model_dropout_rate=0.25,
-        model_device="cpu",
-        model_num_hidden_layers=1,
-        model_batch_size=32,
-        model_epochs=100,
-        model_gamma=2.0,
-        n_jobs=8,
+    nlpca = ImputeNLPCA(
+        gd_filt,
+        n_jobs=1,
         verbose=1,
-        seed=42,
-        sim_strategy="random_inv_multinom",
-        sim_prop_missing=0.3,
+        sim_prop_missing=0.1,
+        sim_strategy="random_balanced_multinom",
+        model_device="cpu",
+        tune=True,
+        prefix="pgsui_nlpca_test",
+        tune_n_trials=500,
+        model_batch_size=64,
     )
 
-    imputed = vae.fit_transform(ge.genotypes_012)
-    print(imputed)
-    print(imputed.shape)
-    print("Best found parameters:")
-    pprint.pprint(vae.best_params_, indent=4)
+    ge = GenotypeEncoder(gd_filt)
+    X = ge.genotypes_012
 
-    print("Scoring metrics:")
-    pprint.pprint(vae.metrics_, indent=4)
+    # ubp.fit_transform(X)
+    nlpca.fit_transform(X)
 
 
 if __name__ == "__main__":
