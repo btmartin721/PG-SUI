6c98b956ccf0e56368791fc1a717856f54b47dc0
btmartin721 <evobio721@gmail.com>
2025-10-26 22:15:52 -0400

    feat: Update preload.js to improve event handling and maintain consistency

--- pgsui/electron/app/preload.js
+++ pgsui/electron/app/preload.js
@@ -1,15 +1,15 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('pgsui', {
+  detectPgSui: () => ipcRenderer.invoke('detect:pgsui'),
   start: (payload) => ipcRenderer.invoke('pgsui:start', payload),
   stop: () => ipcRenderer.invoke('pgsui:stop'),
-  onLog: (cb) => ipcRenderer.on('pgsui:log', (_e, m) => cb(m)),
-  onExit: (cb) => ipcRenderer.on('pgsui:exit', (_e, m) => cb(m)),
-  onStarted: (cb) => ipcRenderer.on('pgsui:started', (_e, m) => cb(m)),
-  onError: (cb) => ipcRenderer.on('pgsui:error', (_e, m) => cb(m)),
   pickFile: (filters) => ipcRenderer.invoke('pick:file', { filters }),
   pickDir: () => ipcRenderer.invoke('pick:dir'),
   pickSave: (defaultPath) => ipcRenderer.invoke('pick:save', { defaultPath }),
+  onLog: (fn) => ipcRenderer.on('pgsui:log', (_e, d) => fn(d)),
+  onError: (fn) => ipcRenderer.on('pgsui:error', (_e, d) => fn(d)),
+  onExit: (fn) => ipcRenderer.on('pgsui:exit', (_e, d) => fn(d)),
+  onStarted: (fn) => ipcRenderer.on('pgsui:started', (_e, d) => fn(d)),
   defaultCli: () => ipcRenderer.invoke('default:cli'),
-  detectPgSui: () => ipcRenderer.invoke('detect:pgsui'),
 });
