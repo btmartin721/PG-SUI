a472e175ad0def6993da944b28ba7cac2afff566
btmartin721 <evobio721@gmail.com>
2026-01-17 21:46:13 -0500

    feat: increase latent_dim range in ImputeNLPCA for enhanced model flexibility

--- pgsui/impute/unsupervised/imputers/nlpca.py
+++ pgsui/impute/unsupervised/imputers/nlpca.py
@@ -1597,8 +1597,8 @@ class ImputeNLPCA(BaseNNImputer):
         params = {
             "latent_dim": trial.suggest_int(
                 "latent_dim",
-                2,
-                min(32, int(self.num_features_), int(len(self.train_idx_))),
+                4,
+                min(64, int(self.num_features_), int(len(self.train_idx_))),
             ),
             "learning_rate": trial.suggest_float("learning_rate", 1e-4, 3e-2, log=True),
             "dropout_rate": trial.suggest_float("dropout_rate", 0.0, 0.5, step=0.05),
